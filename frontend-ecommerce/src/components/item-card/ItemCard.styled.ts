25b6e87a13e63070374b9376b9abbd4b449bf6aa
armeono <arman.hadzigrahic@zendev.se>
2022-09-08 17:07:24 +0200

    working on item card

--- frontend-ecommerce/src/components/item-card/ItemCard.styled.ts
+++ frontend-ecommerce/src/components/item-card/ItemCard.styled.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 export const ItemCardStyled = styled.div`
-    height: 400px;
-    width: 32%;
-    border: 1px solid red;
-    background-color: ${({theme}) => theme.colors.white};
-    border: none;
-    border-radius: 4px;
-    box-shadow: 1px 3px 15px rgba(0,0,0, 0.1);
-`
+  height: 400px;
+  width: 32%;
+  border: 1px solid red;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: none;
+  border-radius: 4px;
+  box-shadow: 1px 3px 15px rgba(0, 0, 0, 0.1);
+`;
 
 export const CardImage = styled.img`
-    
-`
+  width: 100%;
+`;

757498abf1464e3a14bd7c595527c96a35df51d1
armeono <ahadzigrahic@gmail.com>
2022-09-09 02:04:43 +0200

    working on item card

--- frontend-ecommerce/src/components/item-card/ItemCard.tsx
+++ frontend-ecommerce/src/components/item-card/ItemCard.tsx
@@ -15,7 +15,7 @@ interface ItemCardProps {}
 const ItemCard: FunctionComponent<ItemCardProps> = () => {
   return (
     <ItemCardStyled>
-      <CardImage />
+      <CardImage src="https://firebasestorage.googleapis.com/v0/b/e-commerce-a9979.appspot.com/o/Gear-Dell-XPS-15-OLED-1.webp?alt=media&token=b37e243e-00b8-481c-94fe-31aa3d6e21f3"/>
       <CardContent>
         <ItemInfoContainer>
           <ItemName>Dell XPS 15</ItemName>

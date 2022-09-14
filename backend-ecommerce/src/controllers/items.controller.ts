59142dc6b00b7412e52561d7b0e1fe093572a3c4
armeono <arman.hadzigrahic@zendev.se>
2022-09-14 11:10:52 +0200

    working on cart items

--- backend-ecommerce/src/controllers/items.controller.ts
+++ backend-ecommerce/src/controllers/items.controller.ts
@@ -18,8 +18,8 @@ export const createNewItem = () => {
 
   prisma.images.create({
     data: {
-      image_url: "https://firebasestorage.googleapis.com/v0/b/e-commerce-a9979.appspot.com/o/Microsoft%20Surface%2Fc73cd750-ed07-11ec-abdf-7b1af5b1e683.cf.jpg?alt=media&token=5ad1ae7e-6364-4f2a-baf1-bdd22b17c70e",
-      itemsId: 4
+      image_url: "https://firebasestorage.googleapis.com/v0/b/e-commerce-a9979.appspot.com/o/Macbook%2016%2Fvpavic_191118_3800_0122.jpg?alt=media&token=563ebe62-cdb7-4f47-bdfd-15e3795298b8",
+      itemsId: 1
     }
   }).then(() => console.log('item created'))
 
